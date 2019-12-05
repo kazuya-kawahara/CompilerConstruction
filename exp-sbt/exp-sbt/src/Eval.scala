70f2114fd75cca646d85e0396d914b8af0b04f06
kazuya <kawahara.k.ad@m.titech.ac.jp>
2019-12-05 19:11:22 +0900

    revised

--- exp-sbt/exp-sbt/src/Eval.scala
+++ exp-sbt/exp-sbt/src/Eval.scala
@@ -4,7 +4,15 @@ import Base._
 object Eval {
   def eval (env: Map[Var, Int], e: Exp) : Int =
     e match {
-      case VarExp(x) => env(x)
+      case VarExp(x) => {
+        def option2int(x:Option[Int]) = {
+          x match {
+            case Some(i) => i
+            case None => 0
+          }
+        }
+        option2int(env.get(x))
+      } //revised
       case IntExp(i) => i
       case PlusExp(e1, e2) => eval (env, e1) + eval (env, e2)
       case MinusExp(e1, e2) => eval (env, e1) - eval (env, e2)
